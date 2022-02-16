30edf522d5a300d5029a2ae94930c87733000b88
AsadxLion <asad.richardson@meredith.com>
2022-02-16 17:37:36 -0500

    Init commit Svelte skeleton: * Included scss capability * Set up component folder

---
+++ src/App.svelte
@@ -0,0 +1,34 @@
+<script>
+	export let name;
+
+	import Hello from './components/hello.svelte'
+</script>
+
+<main>
+	<Hello msg={'DDM Planning Poker Team!'}/>
+	<h1>and hello {name}</h1>
+	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+</main>
+
+<style lang="scss">
+	$test-scss-var: blue;
+	main {
+		text-align: center;
+		padding: 1em;
+		max-width: 240px;
+		margin: 0 auto;
+	}
+
+	h1 {
+		color: $test-scss-var;
+		text-transform: uppercase;
+		font-size: 4em;
+		font-weight: 100;
+	}
+
+	@media (min-width: 640px) {
+		main {
+			max-width: none;
+		}
+	}
+</style>
---
+++ src/components/hello.svelte
@@ -0,0 +1,14 @@
+	<script>
+  export let msg;
+  </script>
+  <h1>Hello {msg}!</h1>
+
+<style lang="scss">
+  $test-scss-var: red;
+	h1 {
+		color: $test-scss-var;
+		text-transform: uppercase;
+		font-size: 4em;
+		font-weight: 100;
+	}
+</style>
