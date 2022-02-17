b029c538e35c35b4f04ce24aadf01a35a6d63284
gjeelal <gjeelal@dotdash.com>
2022-02-17 14:23:20 -0700

    block-chain api data

---
+++ blockchain/src/index.js
@@ -0,0 +1,9 @@
+import 'regenerator-runtime/runtime'
+
+import { initContract, login, logout } from './utils'
+
+import getConfig from './config'
+const { networkId } = getConfig(process.env.NODE_ENV || 'development')
+
+// global variable used throughout
+let currentGreeting
