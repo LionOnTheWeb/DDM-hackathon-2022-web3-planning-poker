7760540446ad59be35a53a0247b23281fc10c9b2
LionOnTheWeb <asad@hasshdigital.com>
2022-02-17 14:13:58 -0500

    Added routing via svelte-navigator

--- src/App.svelte
+++ src/App.svelte
@@ -1,45 +1,61 @@
 <script>
-	export let name;
+	// export let name;
 
 	import Hello from './components/hello.svelte'
+	import { Router, Link, Route } from "svelte-navigator";
+	import Home from "./routes/Home.svelte";
+	import Planning from "./routes/Plan.svelte";
 </script>
 
-<main>
+
+<Router>
+		<nav>
+			<Link to="/">Home</Link>
+		</nav>
+		<div>
+			<Route path="/">
+				<Home/>
+			</Route>
+			<Route path="planning" component={Planning}/>
+		</div>
+</Router>
+
+<!-- <main>
 	<Hello msg={'DDM Planning Poker Team!'}/>
 	<h1>and hello {name}</h1>
 	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
 	<form>
 	<input type="text"/>
 	</form>
-</main>
+</main> -->
 
 
 <style lang="scss">
-	$test-scss-var: palegreen;
+	// $test-scss-var: palegreen;
 
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
+	// main {
+	// 	text-align: center;
+	// 	padding: 1em;
+	// 	max-width: 240px;
+	// 	margin: 0 auto;
+	// }
 
-	h1 {
-		color: $test-scss-var;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
+	// h1 {
+	// 	color: $test-scss-var;
+	// 	text-transform: uppercase;
+	// 	font-size: 4em;
+	// 	font-weight: 100;
+	// }
 
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
+	// @media (min-width: 640px) {
+	// 	main {
+	// 		max-width: none;
+	// 	}
+	// }
 
-	form {
-		background-color: blue;
-		height: 400px;
-		width: 500px;
-	}
+	// form {
+	// 	background-color: blue;
+	// 	height: 400px;
+	// 	width: 500px;
+	// }
 </style>
---
+++ src/routes/Home.svelte
@@ -0,0 +1,6 @@
+<script>
+import Hello from '../components/hello.svelte'
+</script>
+
+<h1>Home Route</h1>
+<Hello msg="world" />
