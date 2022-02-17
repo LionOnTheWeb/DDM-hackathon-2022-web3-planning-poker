16dafb8afcb35dac86f5dcfb7e5db4b3a727a105
LionOnTheWeb <asad@hasshdigital.com>
2022-02-17 15:32:14 -0500

    Added input component

---
+++ src/components/Input.svelte
@@ -0,0 +1,12 @@
+<script>
+export let label = ""
+export let type = ""
+export let placeholder = null
+
+</script>
+
+{#if label}
+  <label>{label}</label>
+{/if}
+
+<input type="{type}" placeholder="{placeholder}" />
--- src/components/hello.svelte
+++ src/components/hello.svelte
@@ -1,7 +1,11 @@
 	<script>
   export let msg;
+
+  import Input from './Input.svelte'
   </script>
+
   <h1>Hello {msg}!</h1>
+  <Input type="text" label="Facilitator"/>
 
 <style lang="scss">
 
