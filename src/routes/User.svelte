12c93d8519746aba1cf66654f423f307c71c1e08
LionOnTheWeb <asad@hasshdigital.com>
2022-02-18 12:29:34 -0500

    small updates to userpage

--- src/routes/User.svelte
+++ src/routes/User.svelte
@@ -1,18 +1,23 @@
 <script>
   import Card from '../components/Card.svelte';
-  export let userName;
+
+  export let userName = "";
+
   let numbers = [0,1,2,3,5,8,13,21];
-  </script>
-      <div class="header">
-          <h2>Hello {userName}!</h2>
-      </div>
-      <p>Player name</p>
-      <p>Acive Ticket title</p>
-      <div class="card">
-      {#each numbers as number}
-          <Card userNumber={number}/>
-      {/each}
-      </div>
+</script>
+
+  <div class="header">
+      <h2>Hello {userName}!</h2>
+  </div>
+  
+  <p>{userName || "Player name"}</p>
+  <p>Acive Ticket title</p>
+  
+  <div class="card">
+  {#each numbers as number}
+      <Card userNumber={number}/>
+  {/each}
+  </div>
 
 <style lang="scss">
     h2 {
